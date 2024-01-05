ddc6896459e7314d4d4b790d06aef3c5a3944ae7
Yahyabonyguda <120451274+Yahyabonyguda@users.noreply.github.com>
2024-01-05 14:41:49 +0100

    Assignments

---
+++ Day_0002/day002.py
@@ -0,0 +1,27 @@
+# Day 2: 30 Days of python programming  # Comment
+
+# Create variables
+first_name = "John"
+last_name = "Doe"
+full_name = f"{first_name} {last_name}"  # String formatting
+country = "USA"
+city = "New York"
+age = 30
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+
+# Multiple variables on one line
+name, age, country = "Jane", 25, "France"  # Assigning multiple values at once
+
+# Print information
+print(f"Full name: {full_name}")
+print(f"Country: {country}")
+print(f"City: {city}")
+print(f"Age: {age}")
+print(f"Year: {year}")
+print(f"Married: {is_married}")
+print(f"True: {is_true}")
+print(f"Light on: {is_light_on}")
+print(f"Another person: {name}, {age}, {country}")
